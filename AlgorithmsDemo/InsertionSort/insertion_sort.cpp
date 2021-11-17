eb3eaba4e7ac6cf01df38907407cfe9f6e252f8d
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-11-17 09:47:37 -0500

    Create insertion_sort.cpp

---
+++ AlgorithmsDemo/InsertionSort/insertion_sort.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file insertion_sort.cpp
+ * @author Xuhua Huang (xuhuahuang0412@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+#include <stdlib.h>
+
+int main(void) {
+    
+    system("pause");
+    return 0;
+}
