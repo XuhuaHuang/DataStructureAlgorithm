08cfd5f9ab8b1ad2b41b4b5cee8539053aaa5b19
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-03-13 08:50:39 -0400

    Create main.cpp

---
+++ GenericDataStructures/DoubleLinkedList/main.cpp
@@ -0,0 +1,22 @@
+/*****************************************************************//**
+ * \file   main.cpp
+ * \brief  Generic doubly linked list implementation test cases.
+ * 
+ * Consider "../LinkedList/main.cpp"
+ * 
+ * \author Xuhua Huang
+ * \date   December 13, 2022
+ *********************************************************************/
+
+#include <iostream>
+#include <stdlib.h>
+#include <denode.hpp>
+
+auto main(void) -> int {
+    using util::data_structure::denode;
+
+    denode<int> node;
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
